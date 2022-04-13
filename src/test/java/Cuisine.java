d923194fa435472ebe1cd88ffc4d97918759261d
Zadrzavr <kirillmoroz42@gmail.com>
2022-04-13 17:14:14 +0700

    Commit

--- src/test/java/Cuisine.java
+++ src/test/java/Cuisine.java
@@ -1,5 +1,10 @@
 import extensions.SpoonApiTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,16 +15,30 @@ import static io.restassured.RestAssured.given;
 
 @SpoonApiTest
 public class Cuisine {
+    private static RequestSpecification requestSpecification;
+    private static ResponseSpecification responseSpecification;
+
+    @BeforeAll
+    static void beforeAll() {
+        requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("offset", 0)
+                .addQueryParam("confidence", 0.0F)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectBody("confidence", Matchers.equalTo(0.0F))
+                .expectBody("offset", Matchers.equalTo(null))
+                .build();
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"Mediterranean", "Italian"})
     public void cuisineTest(String queryParameter) {
         given()
-                .queryParams(Map.of("query", queryParameter,
-                        "offset", 0,
-                        "confidence", 0.0))
+                .queryParams("query", queryParameter)
+                .spec(requestSpecification)
                 .post("/recipes/cuisine")
                 .then()
+                .spec(responseSpecification)
                 .statusCode(200);
 
     }
--- src/test/java/FoodTest.java
+++ src/test/java/FoodTest.java
@@ -1,5 +1,10 @@
 import extensions.SpoonApiTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,18 +15,32 @@ import static io.restassured.RestAssured.given;
 
 @SpoonApiTest
 public class FoodTest {
+    private static RequestSpecification requestSpecification;
+    private static ResponseSpecification responseSpecification;
 
+    @BeforeAll
+    static void beforeAll() {
+        requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("offset", 0)
+                .addQueryParam("number", 10)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectBody("limit", Matchers.equalTo(10))
+                .expectBody("offset", Matchers.equalTo(0))
+                .build();
+    }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Pizza", "Sushi"})
+    @ValueSource(strings = {"pizza", "Sushi"})
     public void foodSearchTest(String queryParameter) {
         given()
-                .queryParams(Map.of("query", queryParameter,
-                        "offset", 0,
-                        "number", 10))
+                .queryParams("query", queryParameter)
+                .spec(requestSpecification)
                 .get("/food/search")
                 .then()
+                .spec(responseSpecification)
                 .statusCode(200)
                 .body("query", Matchers.equalTo(queryParameter));
+
     }
 }
